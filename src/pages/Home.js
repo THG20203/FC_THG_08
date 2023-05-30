6a4840e0693ce678d1b9be70c1efa8adcee15ba8
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-30 19:35:29 +0100

    debugging classes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Home.module.css";
 
 function HomePage() {
-  return <div className="home-container">My Home Page</div>;
+  return <div className={classes.homeContainer}>My Home Page</div>;
 }
 
 export default HomePage;
