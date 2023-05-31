8805912f60e6632a74b2fd570da25ff11b618a26
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-31 22:58:50 +0100

    second navbar component added

---
+++ src/components/SecondNavbar.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "./SecondNavbar.scss";
+
+function SecondNavbar() {
+  return (
+    <div className="second-nav__container">
+      {/* logo of the football team */}
+      <img
+        className="second-nav__badge"
+        src="img/identity/football-badge.png"
+        alt="badge"
+      />
+      {/* styled burger menu for smaller devices */}
+      <input
+        type="checkbox"
+        className="second-nav__checkbox"
+        id="navi-toggle"
+      />
+      <label for="navi-toggle" className="second-nav__button">
+        <span className="second-nav__icon">&nbsp;</span>
+      </label>
+      {/* background behind menu toggle */}
+      <div className="second-nav__background">&nbsp;</div>
+      {/* Specific second nav */}
+      <nav className="second-nav__content">
+        {/* list of items to select in second navbar */}
+        <ul className="second-nav__list">
+          <li className="second-nav__item">
+            <a href="#" className="second-nav__link">
+              Latest
+            </a>
+          </li>
+          <li className="second-nav__item">
+            <a href="#" className="second-nav__link">
+              Matches
+            </a>
+          </li>
+          <li className="second-nav__item">
+            <a href="#" className="second-nav__link">
+              Stadium
+            </a>
+          </li>
+          <li className="second-nav__item">
+            <a href="#" className="second-nav__link">
+              Culture
+            </a>
+          </li>
+          <li className="second-nav__item">
+            <a href="#" className="second-nav__link">
+              Tickets
+            </a>
+          </li>
+          <li className="second-nav__item">
+            <a href="#" className="second-nav__link">
+              Shop
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default SecondNavbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,7 @@ import React from "react";
 import "./Home.scss";
 
 function HomePage() {
-  return;
-  <div className="home-container">My Home Page</div>;
+  return <div className="home-container">My Home Page</div>;
 }
 
 export default HomePage;
