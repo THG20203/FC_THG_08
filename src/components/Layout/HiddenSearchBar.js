374564b9203e58b99a89cbf5f1f0a501c859576e
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-05 20:01:41 +0100

    beginning useState for hiddensearchicons

--- src/components/Layout/HiddenSearchBar.js
+++ src/components/Layout/HiddenSearchBar.js
@@ -6,6 +6,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuestion } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
+  // useState Code
+  const [uiProps, setUiProps] = useState({
+    background: "#0a19a1",
+    shadow: "",
+    transition: "all .3s ease",
+    opacity: 0,
+    showSearchIcon: true,
+  });
+
+  //JSX code
   return (
     <div className="hidden-searchbar__container">
       <input
@@ -13,10 +23,13 @@ const Sidebar = () => {
         type="text"
         placeholder="Search..."
       />
-      <FontAwesomeIcon
-        className="hidden-searchbar__icon"
-        icon={faQuestion}
-      ></FontAwesomeIcon>
+      {/* if uiProps doesn't exist the search bar disappears */}
+      {!uiProps.showSearchIcon && (
+        <FontAwesomeIcon
+          className="hidden-searchbar__icon"
+          icon={faQuestion}
+        ></FontAwesomeIcon>
+      )}
     </div>
   );
 };
