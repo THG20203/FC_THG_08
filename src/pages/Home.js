eada9a4b19aa33e5b03f80715d84debf48077236
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-05 23:02:13 +0100

    removal of useState from home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Home.scss";
 import Navbar from "../components/Layout/Navbar";
 import HiddenSearchBar from "../components/Layout/HiddenSearchBar";
