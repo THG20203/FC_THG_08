c703a40576b666868fb5acac01d61bbdb6b45068
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-07 09:47:08 +0100

    adding features section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import "./Home.scss";
 import IconNav from "../components/Navigation/IconNav";
 import Navbar from "../components/Navigation/Navbar";
 import Carousel from "../components/Carousel/Carousel";
+import Features from "../components/Features/Features";
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
       <IconNav />
       <Navbar />
       <Carousel />
+      <Features />
     </div>
   );
 };
