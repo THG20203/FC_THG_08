570737313a8c5e157c62e80e14452f2312ab4f40
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-30 17:31:14 +0100

    errors and validation comments

--- backend/util/errors.js
+++ backend/util/errors.js
@@ -1,8 +1,13 @@
 class NotFoundError {
+  /* NotFoundError class is defined with a constructor that takes a message parameter. 
+  It sets the message property of the NotFoundError instance to the provided message 
+  value and assigns the status property to 404. */
   constructor(message) {
     this.message = message;
     this.status = 404;
   }
 }
 
+/* The NotFoundError class is then exported using exports.NotFoundError, which 
+suggests that this code is intended for use in a Node.js environment. */
 exports.NotFoundError = NotFoundError;
