053c9589d9f8333ab56428c2fbebf119446cef43
THG20203 <tristanhgriffiths8@yahoo.com>
2023-07-02 16:03:48 +0100

    base files created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import HomePage from "./pages/Home";
+import HomePage from "./pages/HomePage";
 /* styles */
 import "./index.css";
 import "./assets/styles/typography.scss";
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Home.scss";
+import "./HomePage.scss";
 import IconNav from "../components/Navigation/IconNav";
 import Navbar from "../components/Navigation/Navbar";
 import Carousel from "../components/Carousel/Carousel";
