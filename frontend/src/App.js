17744a022fd445558666291cbdc768f33c26d395
THG20203 <tristanhgriffiths8@yahoo.com>
2023-07-02 22:44:04 +0100

    first progress with route code on the app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import HomePage from "./pages/Home";
+import EventsPage from "./pages/Events";
+import EventDetailPage from "./pages/EventDetail";
+import CulturePage from "./pages/Culture";
+
 /* styles */
 import "./index.css";
 import "./assets/styles/typography.scss";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/events", element: <EventsPage /> },
+  { path: "/events/:eventId", element: <EventDetailPage /> },
+  { path: "/events/new", elements: <NewEventPage />}
+  { path: "/culture", element: <CulturePage /> },
+]);
 
 function App() {
   return (
