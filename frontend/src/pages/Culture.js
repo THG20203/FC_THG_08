6effe8cb6302167a2a195a4735712888dda20d7a
THG20203 <tristanhgriffiths8@yahoo.com>
2023-07-02 17:27:23 +0100

    starting culture page and scss

---
+++ frontend/src/pages/Culture.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Culture.scss";
+import IconNav from "../components/Navigation/IconNav";
+import Navbar from "../components/Navigation/Navbar";
+
+const CulturePage = () => {
+  return (
+    <div className="culture-container">
+      <IconNav />
+      <Navbar />
+    </div>
+  );
+};
+
+export default CulturePage;
