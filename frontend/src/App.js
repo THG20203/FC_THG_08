c937e9912d4fe0902bfd0464484db2602552a254
THG20203 <tristanhgriffiths8@yahoo.com>
2023-07-06 12:56:59 +0100

    beginning routes reassessment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-/* page imports */
+// PAGE IMPORTS
+/* Home Page */
 import HomePage from "./pages/Home";
+/* Events Page */
 import EventsPage from "./pages/Events";
-import EventDetailPage from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEvent";
-import EditEventPage from "./pages/EditEvent";
 import CulturePage from "./pages/Culture";
 
 /* styles */
@@ -15,9 +14,6 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/events", element: <EventsPage /> },
-  { path: "/events/:eventId", element: <EventDetailPage /> },
-  { path: "/events/new", element: <NewEventPage /> },
-  { path: "/evemts/:eventId/edit", element: <EditEventPage /> },
   { path: "/culture", element: <CulturePage /> },
 ]);
 
