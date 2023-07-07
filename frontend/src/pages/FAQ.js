bcd6626b7c9bd38f54a53fa085a8ef077ed1958d
THG20203 <tristanhgriffiths8@yahoo.com>
2023-07-07 11:01:38 +0100

    faq boxes

--- frontend/src/pages/FAQ.js
+++ frontend/src/pages/FAQ.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./FAQ.scss";
+/* For font awesome icons */
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {} from "@fortawesome/free-solid-svg-icons";
 
 const FaqPage = () => {
-  return <h1>Faq test</h1>;
+  return;
+  <>
+    <h1 className="heading-1">Frequently Asked Questions</h1>
+    <div class="faq-container">
+      {/* whichever ones are active have a class of active */}
+      <div class="faq active"></div>
+    </div>
+  </>;
 };
 
 export default FaqPage;
