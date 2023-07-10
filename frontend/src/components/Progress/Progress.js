5b7812bfdd1760850d28c946654e574f57e4e4fa
THG20203 <tristanhgriffiths8@yahoo.com>
2023-07-10 15:02:19 +0100

    adding markup progress bars

--- frontend/src/components/Progress/Progress.js
+++ frontend/src/components/Progress/Progress.js
@@ -1,6 +1,22 @@
 import React from "react";
 import "./Progress.scss";
 
-const Progress = () => {};
+const Progress = () => {
+  <div className="progress-content">
+    <div className="progress-container">
+      <div className="progress" id="progress"></div>
+      <div className="circle active">1</div>
+      <div className="circle">2</div>
+      <div className="circle">3</div>
+      <div className="circle">4</div>
+    </div>
+    <button className="btn" id="prev">
+      Previous
+    </button>
+    <button className="btn" id="next">
+      Next
+    </button>
+  </div>;
+};
 
 export default Progress;
