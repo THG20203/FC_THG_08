c632ce87882d5bb1be6d12fa82b2405a771f0f9d
THG20203 <tristanhgriffiths8@yahoo.com>
2023-07-11 22:15:29 +0100

    importing component

--- frontend/src/pages/whiteNavPages/MyClub.js
+++ frontend/src/pages/whiteNavPages/MyClub.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./MyClub.scss";
+import ExpenseItem from "../../components/MyClub/ExpenseItem";
 
 const MyClub = () => {
   return (
     <div className="my-club__container">
-      <div className="">MyClub test</div>
+      <div>MyClub test</div>
+      <ExpenseItem />
     </div>
   );
 };
