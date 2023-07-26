26d29d3aea899e1dc9f42b75d75a58dc28c237d6
THG20203 <tristanhgriffiths8@yahoo.com>
2023-07-26 22:01:12 +0100

    props for expenses

--- frontend/src/components/MyClub/Card.js
+++ frontend/src/components/MyClub/Card.js
@@ -2,6 +2,10 @@ import React from "react";
 
 import "./Card.scss";
 
-const Card = () => {};
+const Card = (props) => {
+  const classes = "card " + props.className;
+
+  return <div className={classes}>{props.children}</div>;
+};
 
 export default Card;
