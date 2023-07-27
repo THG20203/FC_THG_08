de5347517e5bf7db381548bbb7646e78d4e3dcf7
THG20203 <tristanhgriffiths8@yahoo.com>
2023-07-27 10:36:13 +0100

    sorting date comment explanations

--- frontend/src/components/MyClub/ExpenseDate.js
+++ frontend/src/components/MyClub/ExpenseDate.js
@@ -3,6 +3,10 @@ import React from "react";
 import "./ExpenseDate.scss";
 
 const ExpenseDate = (props) => {
+  {
+    /* toLocaleString -> first argument is the language, second argument is an object, I configure
+    how specifically that day should be formatted, i.e. month or day. */
+  }
   const month = props.date.toLocaleString("en-GB", { month: "long" });
   const day = props.date.toLocaleString("en-GB", { day: "2-digit" });
   const year = props.date.getFullYear();
