ae0b725cd72f39c199c5252323b3230b819e4bd5
THG20203 <tristanhgriffiths8@yahoo.com>
2023-07-30 23:10:30 +0100

    sorting media queries for contacts

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -9,12 +9,12 @@ const contacts = [
   {
     title: "Membership Support",
     description:
-      "Are you a proud member of FC THG08? Our Membership Support team is available to address any queries or concerns related to your membership experience. Whether you need assistance with renewals, benefits, or personalized club merchandise, we're here to provide the help you need.",
+      "Are you a proud member of FC THG08? Our Membership Support team is available to address any queries related to your membership experience. Whether you need assistance with renewals, benefits, or personalized club merchandise, we're here to provide help.",
   },
   {
     title: "Media and Press Enquiries",
     description:
-      "Members of the media and press can get in touch with FC THG08's media relations team for official statements, interview requests, or press credentials. We value our relationship with the media and are eager to collaborate on sharing the latest news, updates, and player interviews. ",
+      "Members of the media and press can get in touch with our media relations team for official statements, interview requests, or press credentials. We value our relationship with the media and are eager to collaborate on sharing the latest news, updates, and player interviews. ",
   },
 ];
 
@@ -32,6 +32,14 @@ const Contact = () => {
         <li className="contact-list">
           <h4 className="heading-4">{contacts[0].title}</h4>
           <p className="paragraph-text">{contacts[0].description}</p>
+        </li>
+        <li className="contact-list">
+          <h4 className="heading-4">{contacts[1].title}</h4>
+          <p className="paragraph-text">{contacts[1].description}</p>
+        </li>
+        <li className="contact-list">
+          <h4 className="heading-4">{contacts[2].title}</h4>
+          <p className="paragraph-text">{contacts[2].description}</p>
         </li>
       </ul>
     </div>
