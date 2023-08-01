a9954acd6ae52a174afa4b4ebc72da6c563b5f58
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-01 21:59:30 +0100

    using more than one state beginning

--- frontend/src/components/Expenses/NewExpense/ExpenseForm.js
+++ frontend/src/components/Expenses/NewExpense/ExpenseForm.js
@@ -6,17 +6,18 @@ const ExpenseForm = () => {
   /* then use destructuring to get our two elements, currently entered title and 
   following the convention 'setEnteredTitle' (a function for updating the state). */
   const [enteredTitle, setEnteredTitle] = useState("");
+  /* you can have multiple states per component */
+  const [enteredAmount, setEnteredAmount] = useState("");
 
-  /* because we pass titleChangeHandler function to react through onChange prop,
-  React will make sure we will get event object when change event occurs  */
-  /* 'target' field in object -> points to dom element for which event occured,
-  and input element in turn has long list of properties which I can read and set. */
-  /* Most important of target -> input elements is the value property */
-  /* value property holds the current value of this input at the point in time this event
-  occurs. */
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
   };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {};
 
   return (
     <form>
@@ -36,11 +37,17 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            onChange={amountChangeHandler}
           />
         </div>
         <div className="new-expense__control">
           <label className="heading-4">Date of Purchase</label>
-          <input className="paragraph-text" type="date" min="2019-01-01" />
+          <input
+            className="paragraph-text"
+            type="date"
+            min="2019-01-01"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
@@ -53,3 +60,12 @@ const ExpenseForm = () => {
 };
 
 export default ExpenseForm;
+
+// useState notes :
+/* because we pass titleChangeHandler function to react through onChange prop,
+  React will make sure we will get event object when change event occurs  */
+/* 'target' field in object -> points to dom element for which event occured,
+  and input element in turn has long list of properties which I can read and set. */
+/* Most important of target -> input elements is the value property */
+/* value property holds the current value of this input at the point in time this event
+  occurs. */
