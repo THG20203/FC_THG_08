2fdf8ad85b00209d26b001aa22730ecb8490890a
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-01 10:57:29 +0100

    removing media query for 100vh - 16rem

--- frontend/src/pages/blueNavPages/FAQ.js
+++ frontend/src/pages/blueNavPages/FAQ.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FAQ.scss";
-/* For font awesome icons */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown, faTimes } from "@fortawesome/free-solid-svg-icons";
 
+/* introduced the activeIndex state variable using the useState hook to keep track of 
+the active FAQ box. When a user clicks on a FAQ box, the toggleFaqBox function is 
+called with the index of that box as an argument, and it toggles the active state by 
+updating the activeIndex. */
+/* Also conditionally apply the "active" class to the FAQ box based on whether its 
+index matches the activeIndex state. Similarly, we show/hide the chevron and times 
+icons based on the active state using the "hidden" class. */
+
 const FaqPage = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const toggleFaqBox = (index) => {
+    setActiveIndex((prevActiveIndex) =>
+      prevActiveIndex === index ? null : index
+    );
+  };
+
   return (
     <section className="faq__section">
       <h1 className="faq__header heading-1--dark">
         Frequently Asked Questions
       </h1>
 
-      {/* FAQ question 1 */}
       <div className="faq-container">
-        <div className="faq-box active">
+        {/* FAQ question 1 */}
+        <div
+          className={`faq-box ${activeIndex === 0 ? "active" : ""}`}
+          onClick={() => toggleFaqBox(0)}
+        >
           <h3 className="faq-box__title heading-3 mb-sm">
             Where can I buy a season ticket?
           </h3>
@@ -24,18 +42,25 @@ const FaqPage = () => {
           </p>
           <button className="faq-toggle">
             <FontAwesomeIcon
-              className="faq-toggle__chevron-down"
+              className={`faq-toggle__chevron-down ${
+                activeIndex === 0 ? "hidden" : ""
+              }`}
               icon={faChevronDown}
             ></FontAwesomeIcon>
             <FontAwesomeIcon
-              className="faq-toggle__times"
+              className={`faq-toggle__times ${
+                activeIndex === 0 ? "" : "hidden"
+              }`}
               icon={faTimes}
             ></FontAwesomeIcon>
           </button>
         </div>
 
         {/* FAQ question 2 */}
-        <div className="faq-box">
+        <div
+          className={`faq-box ${activeIndex === 1 ? "active" : ""}`}
+          onClick={() => toggleFaqBox(1)}
+        >
           <h3 className="faq-box__title heading-3 mb-sm">
             What games are included in the season ticket?
           </h3>
@@ -45,32 +70,43 @@ const FaqPage = () => {
           </p>
           <button className="faq-toggle">
             <FontAwesomeIcon
-              className="faq-toggle__chevron-down"
+              className={`faq-toggle__chevron-down ${
+                activeIndex === 1 ? "hidden" : ""
+              }`}
               icon={faChevronDown}
             ></FontAwesomeIcon>
             <FontAwesomeIcon
-              className="faq-toggle__times"
+              className={`faq-toggle__times ${
+                activeIndex === 1 ? "" : "hidden"
+              }`}
               icon={faTimes}
             ></FontAwesomeIcon>
           </button>
         </div>
 
         {/* FAQ question 3 */}
-        <div className="faq-box">
+        <div
+          className={`faq-box ${activeIndex === 2 ? "active" : ""}`}
+          onClick={() => toggleFaqBox(2)}
+        >
           <h3 className="faq-box__title heading-3 mb-sm">
             How do I sign up as a club member?
           </h3>
           <p className="faq-box__text paragraph-text">
             For information and how to purchase a club membership, please visit
-            the sign up page.
+            the sign-up page.
           </p>
           <button className="faq-toggle">
             <FontAwesomeIcon
-              className="faq-toggle__chevron-down"
+              className={`faq-toggle__chevron-down ${
+                activeIndex === 2 ? "hidden" : ""
+              }`}
               icon={faChevronDown}
             ></FontAwesomeIcon>
             <FontAwesomeIcon
-              className="faq-toggle__times"
+              className={`faq-toggle__times ${
+                activeIndex === 2 ? "" : "hidden"
+              }`}
               icon={faTimes}
             ></FontAwesomeIcon>
           </button>
