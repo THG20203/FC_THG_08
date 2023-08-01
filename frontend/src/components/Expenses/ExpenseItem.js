579606c722068a5536695c557dddcf8a3111c266
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-01 12:05:52 +0100

    first notes on useState

--- frontend/src/components/Expenses/ExpenseItem.js
+++ frontend/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 import "./ExpenseItem.scss";
 
-{
-  /* How do we get the values defined in the place where we use our custom component? 
-i.e the parent component -> Expense.js. Answer = parameters */
-  /* rather than writing title, amount, date to represent what we get from the parent 
-component, we write one parameter, props. React will ensure we get one parameter in
-every component we use, like props in ExpenseItem. Typically named props */
-}
-
 const ExpenseItem = (props) => {
+  {
+    /* useState = react hook -> starts with word use, all hooks must only be called inside React 
+    functions like ExpenseItem, and cannot be called inside nested functions in React. */
+    /* useState returns an array where the first value is the variable itself (initial value), second
+    element in the array is updating function. We can use array destructuring to store both 
+    elements in seperate varialbes or consts. Order matters */
+    /* First argument 'title' = pointer at that managed value, */
+  }
+  const [title, setTitle] = useState();
+
   return (
     <Card className="expense-item">
       {/* Set the date prop -> used props.date in ExpenseDate.js. Think where the date data is,
@@ -28,3 +30,12 @@ const ExpenseItem = (props) => {
 };
 
 export default ExpenseItem;
+
+{
+  // PROPS NOTES:
+  /* How do we get the values defined in the place where we use our custom component? 
+i.e the parent component -> Expense.js. Answer = parameters */
+  /* rather than writing title, amount, date to represent what we get from the parent 
+component, we write one parameter, props. React will ensure we get one parameter in
+every component we use, like props in ExpenseItem. Typically named props */
+}
