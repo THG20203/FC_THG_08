bf075ae981b947a6570049e6c8881846896d20eb
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-02 17:06:42 +0100

    further child to parent comms

--- frontend/src/components/Expenses/NewExpense/NewExpense.js
+++ frontend/src/components/Expenses/NewExpense/NewExpense.js
@@ -3,11 +3,23 @@ import "./NewExpense.scss";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = () => {
+  /* name -> parameter 'enteredExpenseData' = my choice, but now make it clear this function expects 
+  to get this parameter. */
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      /* pull out all the key value pairs and add them to this new object */
+      ...enteredExpenseData,
+      /* then add in new key, the id key \and set it to math.random and convert to a string */
+      id: Math.random().toString();
+    };
+    console.log(expenseData);
+  };
+
   return (
     <div className="new-expense">
       {/* naming it 'on'something cause value for this prop should be a function that will eventually 
-      be triggered when something happens inside of this component -> i.e. when form = submitted. */}
-      <ExpenseForm onSaveExepnseData={} />
+      be triggered when the form = submitted. */}
+      <ExpenseForm onSaveExepnseData />
     </div>
   );
 };
