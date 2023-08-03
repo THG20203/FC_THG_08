72c117e45b76edf3cb2b33d5f0df6aace8f39c3f
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-03 23:15:25 +0100

    debugging console errors

--- frontend/src/components/CultureForm/CultureForm.js
+++ frontend/src/components/CultureForm/CultureForm.js
@@ -2,23 +2,31 @@ import React from "react";
 import "./CultureForm.scss";
 
 const CultureForm = () => {
+  const titleChangeHandler = () => {
+    console.log("Title Changed");
+  };
+
   return (
     <form className="form">
       <div className="form-group">
         <label type="text" className="heading-4">
           First Name:
         </label>
-        <input type="text" className="paragraph-text" />
+        <input
+          onChange={titleChangeHandler}
+          type="text"
+          className="paragraph-text"
+        />
       </div>
 
-      <div class="form-group">
+      <div className="form-group">
         <label type="text" className="heading-4">
           Last Name:
         </label>
         <input type="text" className="paragraph-text" />
       </div>
 
-      <div class="form-group">
+      <div className="form-group">
         <label type="text" className="heading-4">
           Your Fan Story:
         </label>
