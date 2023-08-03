0545cb5597825ee59f61befff30781d747183820
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-03 23:02:58 +0100

    simpilfying html

--- frontend/src/components/CultureForm/CultureForm.js
+++ frontend/src/components/CultureForm/CultureForm.js
@@ -5,42 +5,24 @@ const CultureForm = () => {
   return (
     <form className="form">
       <div className="form-group">
-        <label for="firstName" className="heading-4">
+        <label type="text" className="heading-4">
           First Name:
         </label>
-        <input
-          type="text"
-          id="firstName"
-          name="firstName"
-          required
-          className="paragraph-text"
-        />
+        <input type="text" className="paragraph-text" />
       </div>
 
       <div class="form-group">
-        <label for="lastName" className="heading-4">
+        <label type="text" className="heading-4">
           Last Name:
         </label>
-        <input
-          type="text"
-          id="lastName"
-          name="lastName"
-          required
-          className="paragraph-text"
-        />
+        <input type="text" className="paragraph-text" />
       </div>
 
       <div class="form-group">
-        <label for="message" className="heading-4">
+        <label type="text" className="heading-4">
           Your Fan Story:
         </label>
-        <textarea
-          id="message"
-          name="message"
-          rows="8"
-          required
-          className="paragraph-text"
-        ></textarea>
+        <textarea id="message" rows="8" className="paragraph-text"></textarea>
       </div>
 
       <button type="submit" className="submit-button btn">
