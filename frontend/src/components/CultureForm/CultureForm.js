ac55e92fc97e87bc30a83d6f8680759891d4c312
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-03 16:44:50 +0100

    more basic css styles for now

--- frontend/src/components/CultureForm/CultureForm.js
+++ frontend/src/components/CultureForm/CultureForm.js
@@ -3,23 +3,47 @@ import "./CultureForm.scss";
 
 const CultureForm = () => {
   return (
-    <form className="form">
-      <div className="form-group">
-        <label for="firstName">First Name:</label>
-        <input type="text" id="firstName" name="firstName" required />
+    <form class="form">
+      <div class="form-group">
+        <label for="firstName" className="heading-4">
+          First Name:
+        </label>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          required
+          className="paragraph-text"
+        />
       </div>
 
-      <div className="form-group">
-        <label for="lastName">Last Name:</label>
-        <input type="text" id="lastName" name="lastName" required />
+      <div class="form-group">
+        <label for="lastName" className="heading-4">
+          Last Name:
+        </label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          required
+          className="paragraph-text"
+        />
       </div>
 
-      <div className="form-group">
-        <label for="message">Message:</label>
-        <textarea id="message" name="message" rows="4" required></textarea>
+      <div class="form-group">
+        <label for="message" className="heading-4">
+          Your Fan Story:
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          rows="8"
+          required
+          className="paragraph-text"
+        ></textarea>
       </div>
 
-      <button type="submit" className="submit-button btn">
+      <button type="submit" class="submit-button btn">
         Submit
       </button>
     </form>
