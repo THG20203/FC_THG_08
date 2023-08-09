7720173ae39580b7d5790caf695b06ed892b292a
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-09 15:02:33 +0100

    changes

--- frontend/src/components/News/NewsCard.js
+++ frontend/src/components/News/NewsCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const NewsCard = (props) => {
   return (
     <div className="news-card__content">
-      <div className="news-card__img-div mb-sm">
+      <div className="news-card__img-div">
         <img className="news-card__image" src={props.image} alt={props.alt} />
       </div>
       <h3 className="news-card__heading heading-2 mb-sm">{props.heading}</h3>
--- frontend/src/pages/whiteNavPages/Home.js
+++ frontend/src/pages/whiteNavPages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Home.scss";
 import Carousel from "../../components/Carousel/Carousel";
+import News from "../../components/News/News";
 import Features from "../../components/Features/Features";
 
 const HomePage = () => {
   return (
     <div className="home-container">
       <Carousel />
+      <News />
       <Features />
     </div>
   );
