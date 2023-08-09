50895f33c12099b1def151f4adae58e79d833775
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-09 14:41:30 +0100

    cards sorted with css grid for headlines

--- frontend/src/components/News/NewsCard.js
+++ frontend/src/components/News/NewsCard.js
@@ -3,8 +3,12 @@ import React from "react";
 const NewsCard = (props) => {
   return (
     <div className="news-card__content">
-      <img className="news-card__image" src={props.image} alt={props.alt} />
-      <h3 className="news-card__heading heading-2">{props.heading}</h3>
+      <img
+        className="news-card__image mb-sm"
+        src={props.image}
+        alt={props.alt}
+      />
+      <h3 className="news-card__heading heading-2 mb-sm">{props.heading}</h3>
       <p className="news-card__description paragraph-text">
         {props.description}
       </p>
