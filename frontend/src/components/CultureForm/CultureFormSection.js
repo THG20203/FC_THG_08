5c4d46b9cc6a7e0fc9228a033c03170685895c03
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-11 11:54:00 +0100

    child to parent communication for culture form initial

--- frontend/src/components/CultureForm/CultureFormSection.js
+++ frontend/src/components/CultureForm/CultureFormSection.js
@@ -3,9 +3,17 @@ import "./CultureFormSection.scss";
 import CultureForm from "./CultureForm";
 
 const CultureFormSection = () => {
+  const saveCultureDataHandler = (enteredCultureData) => {
+    /* cultureFormData = object I generate in the submit handler */
+    const cultureFormData = {
+      /* pulling out all the key value pairs */
+      ...enteredCultureData,
+    };
+  };
+
   return (
     <div className="culture-form__container">
-      <CultureForm />
+      <CultureForm onSaveCultureData />
     </div>
   );
 };
