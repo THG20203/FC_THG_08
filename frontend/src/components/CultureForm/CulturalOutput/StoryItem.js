515b58383ff9965c4984193c297ff5df149cf718
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-12 12:25:14 +0100

    starting again with cards transitions were tempremental

--- frontend/src/components/CultureForm/CulturalOutput/StoryItem.js
+++ frontend/src/components/CultureForm/CulturalOutput/StoryItem.js
@@ -2,25 +2,7 @@ import React, { useState } from "react";
 import "./StoryItem.scss";
 
 const StoryItem = (props) => {
-  return (
-    <div className="story-item__card mb-sm">
-      <div className="story-item__face1">
-        <div className="story-item__name">
-          <h3 className="heading-4">Full Name</h3>
-        </div>
-        <div className="story-item__title">
-          <h3 className="heading-3">Fan Story Title</h3>
-        </div>
-      </div>
-      <div className="story-item__face2">
-        <div className="story-item__content">
-          <p className="paragraph-text">
-            Lorem ipsum test text for now to fill this space a bit.
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>Test</div>;
 };
 
 export default StoryItem;
