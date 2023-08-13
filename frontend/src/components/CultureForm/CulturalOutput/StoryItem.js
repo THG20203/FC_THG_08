68bc84594ead1361ad9dfe8a59cfd7cdffa0a6b1
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-13 21:37:01 +0100

    story item card begininng

--- frontend/src/components/CultureForm/CulturalOutput/StoryItem.js
+++ frontend/src/components/CultureForm/CulturalOutput/StoryItem.js
@@ -2,7 +2,17 @@ import React, { useState } from "react";
 import "./StoryItem.scss";
 
 const StoryItem = (props) => {
-  return <div>Test</div>;
+  return (
+    <div className="card">
+      <div className="face face1">
+        <h2 className="heading-2">Services</h2>
+        <p>Lorem ipsum test text.</p>
+      </div>
+      <div className="face face2">
+        <h2>88</h2>
+      </div>
+    </div>
+  );
 };
 
 export default StoryItem;
