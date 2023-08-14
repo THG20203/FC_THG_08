4b669dd4fd5564e098a3f3d4563b33550c2dc626
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-14 16:01:33 +0100

    splitting up culture page content into components

---
+++ frontend/src/components/CultureContent/CultureContent.js
@@ -0,0 +1,39 @@
+import React from "react";
+/* images for culture page */
+import underpass from "../../assets/images/graffiti/underpass.png";
+import mural from "../../assets/images/graffiti/mural.png";
+import "./CultureContent.scss";
+
+const CultureContent = () => {
+  return (
+    <>
+      {/* Story Pictures section */}
+      <div className="story__pictures">
+        <img className="story__img--1" src={underpass} alt="underpass" />
+        <img className="story__img--2" src={mural} alt="mural" />
+      </div>
+
+      {/* Story Content section */}
+      <div className="story__content">
+        <h3 className="heading-3 mb-sm">A piece on our distinct culture...</h3>
+        <h1 className="heading-1--dark mb-md">Graffiti Art</h1>
+        <p className="story__text paragraph-text">
+          Fans of FC THG 08 are known for their strong sense of pride and
+          loyalty to the club, which is often reflected in the graffiti they
+          create around the stadium and throughout the city. These fans believe
+          that graffiti is a powerful way to express their love for the club and
+          to connect with other fans. They often depict the club's crest,
+          players, and name in blue - the traditional colour of FC THG 08.
+          <br />
+          <br />
+          Graffiti is seen as a way to assert the team's identity in the
+          community and to create a sense of belonging among fans. Despite this
+          being considered as vandalism by some, the fans of FC THG 08 see it as
+          a way to add to their club's and city's culture.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default CultureContent;
