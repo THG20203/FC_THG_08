bdfa51184bca90607b68684446bed9d11ec62517
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-14 10:26:10 +0100

    deciding on card colour scheme

--- frontend/src/components/CultureForm/CulturalOutput/StoryItem.js
+++ frontend/src/components/CultureForm/CulturalOutput/StoryItem.js
@@ -3,13 +3,13 @@ import "./StoryItem.scss";
 
 const StoryItem = (props) => {
   return (
-    <div className="card">
+    <div className="story-item__card">
       <div className="face face1">
-        <h2 className="heading-2">Services</h2>
+        <h2 className="heading-2 story-item__heading">Services</h2>
         <p>Lorem ipsum test text.</p>
       </div>
       <div className="face face2">
-        <h2>88</h2>
+        <h2 className="heading-2 story-item__heading">88</h2>
       </div>
     </div>
   );
