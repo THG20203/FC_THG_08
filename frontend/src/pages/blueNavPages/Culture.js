69b0adfabc54a14148c712d31f15efb132f09a9d
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-14 16:18:54 +0100

    test content added

--- frontend/src/pages/blueNavPages/Culture.js
+++ frontend/src/pages/blueNavPages/Culture.js
@@ -4,7 +4,11 @@ import CultureContent from "../../components/CultureContent/CultureContent";
 import CultureFormSection from "../../components/CultureForm/CulturalEntry/CultureFormSection";
 import CulturalStory from "../../components/CultureForm/CulturalOutput/CulturalStory";
 
-const stories = [{ id: "s1", title: , story: , name: "Tristan",}, { id: "s2", title: , story: , name: "Harry",}, { id: "s3", title: ,  story: , name: "Becky"}];
+const stories = [
+  { id: "s1", title: "test", story: "test", name: "Tristan's Story" },
+  { id: "s2", title: "test", story: "test", name: "Harry's Story" },
+  { id: "s3", title: "test", story: "test", name: "Becky's Story" },
+];
 
 const CulturePage = () => {
   return (
