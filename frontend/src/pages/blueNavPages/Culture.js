7c307d6911d360a3c26b2bd042005435e8767724
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-14 16:45:16 +0100

    content added to culture stories

--- frontend/src/pages/blueNavPages/Culture.js
+++ frontend/src/pages/blueNavPages/Culture.js
@@ -5,9 +5,27 @@ import CultureFormSection from "../../components/CultureForm/CulturalEntry/Cultu
 import CulturalStory from "../../components/CultureForm/CulturalOutput/CulturalStory";
 
 const stories = [
-  { id: "s1", title: "test", story: "test", name: "Tristan's Story" },
-  { id: "s2", title: "test", story: "test", name: "Harry's Story" },
-  { id: "s3", title: "test", story: "test", name: "Becky's Story" },
+  {
+    id: "s1",
+    title: "Miracle Comeback",
+    story:
+      "Down 3-0, the team rallied in the final minutes. Fans witnessed a jaw-dropping turnaround, erupting in euphoria. Unforgettable victory.",
+    name: "Tristan",
+  },
+  {
+    id: "s2",
+    title: "Stadium Roar",
+    story:
+      "The stadium quaked as a last-minute goal secured the championship. Deafening cheers resonated, echoing the team's triumph.",
+    name: "Harry",
+  },
+  {
+    id: "s3",
+    title: "Historic Triumph",
+    story:
+      "FC THG 08 claimed the league title after decades. The city painted in team colors, streets celebrating a long-awaited victory parade.",
+    name: "Becky",
+  },
 ];
 
 const CulturePage = () => {
