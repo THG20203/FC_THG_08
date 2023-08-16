6c499acf034fabd243eb35d8657b6920e0793905
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-16 11:14:39 +0100

    adding conditional return statements based on individual component expenseslist

--- frontend/src/components/Expenses/ExpensesOutput/ExpensesList.js
+++ frontend/src/components/Expenses/ExpensesOutput/ExpensesList.js
@@ -6,20 +6,28 @@ const ExpensesList = (props) => {
   /* Cut jsx code down with variables for && operator conditiional statement below */
   let expensesContent = <p className="paragraph-text">No expenses found.</p>;
 
-  /* called items props in Expenses, no longer have filteredItems in this component, that 
-  info is being passed down to ExpensesList from Expenses via items prop. */
-  if (props.items.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
+  /* can use return statements conditionally if the entire JSX content changes,
+  not just a part of it */
+  if (props.items.length === 0) {
+    return (
+      <h2 className="expenses-list__fallback heading-2 mb-sm">
+        Found no expenses
+      </h2>
+    );
   }
 
-  return <ul className="expenses-list"></ul>;
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default ExpensesList;
