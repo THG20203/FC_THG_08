3c1956d690233cab70b408dd96a940342a82d0da
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-17 12:34:10 +0100

    starting overall chart make up and data points definitions

---
+++ frontend/src/components/Expenses/ExpensesOutput/ExpensesChart.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Chart from "../Chart/Chart";
+
+const ExpensesChart = () => {
+  const chartDataPoint = [{ label }];
+
+  return <Chart />;
+};
+
+export default ExpensesChart;
