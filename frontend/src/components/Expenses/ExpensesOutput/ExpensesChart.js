13c29f9cda2a2f631f6431d931e27108dd451729
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-20 13:42:03 +0100

    chartbar scss

--- frontend/src/components/Expenses/ExpensesOutput/ExpensesChart.js
+++ frontend/src/components/Expenses/ExpensesOutput/ExpensesChart.js
@@ -2,19 +2,25 @@ import React from "react";
 import Chart from "../Chart/Chart";
 
 const ExpensesChart = (props) => {
+  /* This property returns the inner width of the browser window's 
+  content area in pixels. It represents the available width for rendering 
+  the webpage content, excluding scrollbars, borders, and other UI elements. */
+  const isMobileView = window.innerWidth < 800;
+
   const chartDataPoints = [
-    { label: "Jan", value: 0 },
-    { label: "Feb", value: 0 },
-    { label: "Mar", value: 0 },
-    { label: "Apr", value: 0 },
-    { label: "May", value: 0 },
-    { label: "Jun", value: 0 },
-    { label: "Jul", value: 0 },
-    { label: "Aug", value: 0 },
-    { label: "Sep", value: 0 },
-    { label: "Oct", value: 0 },
-    { label: "Nov", value: 0 },
-    { label: "Dec", value: 0 },
+    /* what the labels used to be { label: "Jan", value: 0 }, */
+    { label: isMobileView ? "J" : "Jan", value: 0 },
+    { label: isMobileView ? "F" : "Feb", value: 0 },
+    { label: isMobileView ? "M" : "Mar", value: 0 },
+    { label: isMobileView ? "A" : "Apr", value: 0 },
+    { label: isMobileView ? "M" : "May", value: 0 },
+    { label: isMobileView ? "J" : "Jun", value: 0 },
+    { label: isMobileView ? "J" : "Jul", value: 0 },
+    { label: isMobileView ? "A" : "Aug", value: 0 },
+    { label: isMobileView ? "S" : "Sep", value: 0 },
+    { label: isMobileView ? "O" : "Oct", value: 0 },
+    { label: isMobileView ? "N" : "Nov", value: 0 },
+    { label: isMobileView ? "D" : "Dec", value: 0 },
   ];
 
   for (const expense of props.expenses) {
