6d7b26c70bf52aea9a144d2157dd95088d7bb6b6
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-22 19:32:41 +0100

    centering content

--- frontend/src/components/Shop/Items/ItemsSummary.js
+++ frontend/src/components/Shop/Items/ItemsSummary.js
@@ -2,17 +2,19 @@ import "./ItemsSummary.scss";
 
 const ItemsSummary = () => {
   return (
-    <section className="items-summary">
-      <h4 className="heading-4">Delicious Food, Delivered To You</h4>
-      <p className="paragraph-text">
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
-      </p>
-      <p className="paragraph-text">
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
-      </p>
-    </section>
+    <div className="items-summary__container">
+      <section className="items-summary__content">
+        <h4 className="heading-4">Delicious Food, Delivered To You</h4>
+        <p className="paragraph-text">
+          Choose your favorite meal from our broad selection of available meals
+          and enjoy a delicious lunch or dinner at home.
+        </p>
+        <p className="paragraph-text">
+          All our meals are cooked with high-quality ingredients, just-in-time
+          and of course by experienced chefs!
+        </p>
+      </section>
+    </div>
   );
 };
 
