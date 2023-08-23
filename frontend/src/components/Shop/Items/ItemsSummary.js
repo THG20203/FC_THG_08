454f6e10c6738437bf7a90e87319a5d3f0c1cde4
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-23 11:09:01 +0100

    decisions made on css

--- frontend/src/components/Shop/Items/ItemsSummary.js
+++ frontend/src/components/Shop/Items/ItemsSummary.js
@@ -7,12 +7,6 @@ const ItemsSummary = () => {
         <h2 className="items-summary__title heading-2">
           Shop Official Mechandise
         </h2>
-        <p className="items-summary__text paragraph-text">
-          Explore our exclusive collection of official merchandise from FC THG
-          08. From kits to accessories, find it all here within our online club
-          shop. Whether you're at the stadium or watching from home, wear our
-          colours with pride and be a true part of the FC THG 08 community.
-        </p>
       </section>
     </div>
   );
