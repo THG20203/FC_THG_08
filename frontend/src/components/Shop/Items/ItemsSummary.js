1fc6273e5e4b6015165af93278f1fd36aafe2ecf
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-23 10:32:33 +0100

    margin issues but items component ok

--- frontend/src/components/Shop/Items/ItemsSummary.js
+++ frontend/src/components/Shop/Items/ItemsSummary.js
@@ -4,8 +4,10 @@ const ItemsSummary = () => {
   return (
     <div className="items-summary__container">
       <section className="items-summary__content">
-        <h2 className="heading-2 mb-sm">Shop Official Mechandise</h2>
-        <p className="paragraph-text">
+        <h2 className="items-summary__title heading-2">
+          Shop Official Mechandise
+        </h2>
+        <p className="items-summary__text paragraph-text">
           Explore our exclusive collection of official merchandise from FC THG
           08. From kits to accessories, find it all here within our online club
           shop. Whether you're at the stadium or watching from home, wear our
