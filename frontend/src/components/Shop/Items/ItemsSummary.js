a6f404b6664dd566d6c59028f19e0e44a75fc37d
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-24 22:02:43 +0100

    sorted overflow issue

--- frontend/src/components/Shop/Items/ItemsSummary.js
+++ frontend/src/components/Shop/Items/ItemsSummary.js
@@ -5,7 +5,7 @@ const ItemsSummary = () => {
     <div className="items-summary__container">
       <section className="items-summary__content">
         <h2 className="items-summary__title heading-2">
-          Shop Official Mechandise
+          Shop Official Merchandise
         </h2>
       </section>
     </div>
