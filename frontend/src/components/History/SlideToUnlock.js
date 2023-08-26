32d8e1d6c1e75c6fa4a796f6f24ff9eeb5669c65
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-26 16:15:54 +0100

    sorting lock screen general layout

--- frontend/src/components/History/SlideToUnlock.js
+++ frontend/src/components/History/SlideToUnlock.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import LockSlider from "./LockSlider.js";
 import "./SlideToUnlock.scss";
+import { AiFillUnlock } from "react-icons/ai";
+
+//const [showLockSlider, setShowLockSlider] = useState(0);
 
 function SlideToUnlock() {
   return (
     <div className="slide-unlock__container">
+      <h1 className="heading-1 slide-unlock__title">History</h1>
       <LockSlider width={"40vw"} />
+      <AiFillUnlock className="slide-unlock__icon" />
     </div>
   );
 }
