1193db8fd0366014067fa154c9f041c4b479517f
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-27 21:13:09 +0100

    styling the container of the individual progress bars

--- frontend/src/components/MyClub/ProgressTracker/ProgressBar.js
+++ frontend/src/components/MyClub/ProgressTracker/ProgressBar.js
@@ -1,11 +1,17 @@
 export const ProgressBar = () => {
-  const containerStyles = {};
+  const containerStyles = {
+    height: 25,
+    backgroundColor: "var(--color-grey-2)",
+    borderRadius: 50,
+    margin: 50,
+    width: "100%",
+  };
   const fillerStyles = {};
   const labelStyles = {};
   return (
     <div style={containerStyles}>
       <div style={fillerStyles}>
-        <span className="heading-4--dark" style={labelStyles}>
+        <span className="paragraph-text" style={labelStyles}>
           {"Completed"}
         </span>
       </div>
