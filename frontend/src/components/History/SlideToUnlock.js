3d87a960ed7cf147455ddebce52b4bd9b2a269ab
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-27 13:46:45 +0100

    slidetounlock background being edited

--- frontend/src/components/History/SlideToUnlock.js
+++ frontend/src/components/History/SlideToUnlock.js
@@ -2,15 +2,30 @@ import React, { useState } from "react";
 import LockSlider from "./LockSlider.js";
 import "./SlideToUnlock.scss";
 import { AiFillUnlock } from "react-icons/ai";
-
-const [showLockSlider, setShowLockSlider] = useState(0);
+import yearsBanner from "../../assets/images/banners/130-years-banner.png";
 
 function SlideToUnlock() {
+  /* useState for uiLockScreenProps pass in an object -> key value pairs. */
+  const [uiLockScreenProps, setUiLockScreenProps] = useState({
+    uiText: "Explore our History",
+    uiBg: `url(${yearsBanner}) center/cover no-repeat`,
+  });
+
+  const [showLockSlider, setShowLockSlider] = useState(true);
+
   return (
-    <div className="slide-unlock__container">
-      <h1 className="heading-1 slide-unlock__title">History</h1>
-      <LockSlider width={"40vw"} />
-      <AiFillUnlock className="slide-unlock__icon" />
+    <div
+      style={{ background: uiLockScreenProps.uiBg }}
+      className="slide-unlock__container"
+    >
+      <h1 className="heading-1 slide-unlock__title">
+        {uiLockScreenProps.uiText}
+      </h1>
+      {showLockSlider ? (
+        <LockSlider width={"40vw"} />
+      ) : (
+        <AiFillUnlock className="slide-unlock__icon" />
+      )}
     </div>
   );
 }
