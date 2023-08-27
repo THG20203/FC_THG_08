1c7e67f563c4397ee82d6eed773987875b575bb3
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-27 17:03:23 +0100

    thinking about general styles on the my club ui

--- frontend/src/components/MyClub/ProgressTracker/ProgressBars.js
+++ frontend/src/components/MyClub/ProgressTracker/ProgressBars.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./ProgressBars.scss";
 
 const ProgressBars = () => {
-  return <div className="progress-bars">ProgressBars</div>;
+  return (
+    <div className="progress-bars__container">
+      <div className="heading-1 mb-sm">Club Priorities</div>
+    </div>
+  );
 };
 
 export default ProgressBars;
