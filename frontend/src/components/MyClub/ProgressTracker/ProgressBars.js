0562b3097c0cab5299aa8cda587e8ee3f58401d8
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-27 17:54:36 +0100

    styling inputs and labels for club priorities bars

--- frontend/src/components/MyClub/ProgressTracker/ProgressBars.js
+++ frontend/src/components/MyClub/ProgressTracker/ProgressBars.js
@@ -8,9 +8,21 @@ const ProgressBars = () => {
         <div className="heading-1 mb-sm">Club Priorities</div>
         <h4 className="heading-4">
           Please enter how important you consider these topics in terms of your
-          fan experience.
+          fan experience. Your rating will be 0 - 100%, with 100% meaning very
+          important.
         </h4>
       </header>
+      <ul>
+        <li>
+          <h4 className="progress-bars__label-input heading-4--dark">
+            Team Success and Performance
+          </h4>
+          <input
+            className="progress-bars__input paragraph-text"
+            type="number"
+          />
+        </li>
+      </ul>
     </div>
   );
 };
