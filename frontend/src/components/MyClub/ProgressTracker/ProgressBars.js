115c93055e88e8d8074abe10cfae4c1b6d11ae59
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-27 17:28:09 +0100

    margin on description for progress bars completed

--- frontend/src/components/MyClub/ProgressTracker/ProgressBars.js
+++ frontend/src/components/MyClub/ProgressTracker/ProgressBars.js
@@ -4,11 +4,13 @@ import "./ProgressBars.scss";
 const ProgressBars = () => {
   return (
     <div className="progress-bars__container">
-      <div className="heading-1 mb-sm">Club Priorities</div>
-      <h4 className="heading-4">
-        Please enter how important you consider these topics in terms of your
-        fan experience.
-      </h4>
+      <header className="mb-lg">
+        <div className="heading-1 mb-sm">Club Priorities</div>
+        <h4 className="heading-4">
+          Please enter how important you consider these topics in terms of your
+          fan experience.
+        </h4>
+      </header>
     </div>
   );
 };
