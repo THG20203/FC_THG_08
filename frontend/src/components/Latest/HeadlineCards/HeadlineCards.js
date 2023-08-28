8c8006f5bf8d378beea7c854a37fde7551d05c0a
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-28 15:04:27 +0100

    beginning markup for the headline cards

---
+++ frontend/src/components/Latest/HeadlineCards/HeadlineCards.js
@@ -0,0 +1,13 @@
+import "./HeadlineCards.scss";
+
+const HeadlineCards = () => {
+  return (
+    <div className="headline-cards__row">
+      <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default HeadlineCards;
