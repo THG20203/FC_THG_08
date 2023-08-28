78d9b9aad7217ce281260ced2d0d76eb31683614
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-28 16:59:49 +0100

    general markup beginning of latest

--- frontend/src/components/Latest/NotableStories/NotableStories.js
+++ frontend/src/components/Latest/NotableStories/NotableStories.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./NotableStories.scss";
 
 const NotableStories = () => {
-  return <div className="notable-stories__container"></div>;
+  return (
+    <div className="notable-stories__container">
+      <figure className="notable-stories__review">
+        <blockquote className="notable-stories__review-text"></blockquote>
+        <figcaption className="notable-stories__review-user"></figcaption>
+      </figure>
+      <figure className="notable-stories__review">
+        <blockquote className="notable-stories__review-text"></blockquote>
+      </figure>
+    </div>
+  );
 };
 
 export default NotableStories;
