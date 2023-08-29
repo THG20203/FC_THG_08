008d1c69caa154045f5fa64c8d561c98d60623de
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-29 21:55:55 +0100

    text manipulationfounders

--- frontend/src/components/History/HistoryFounder/HistoryFounder.js
+++ frontend/src/components/History/HistoryFounder/HistoryFounder.js
@@ -4,7 +4,7 @@ const HistoryFounder = () => {
   return (
     <div className="history-founder__container">
       <div className="history-founder__content">
-        <p className="history-founder__text paragraph-text mb-sm">
+        <p className="paragraph-text mb-sm">
           In the late 19th century, amidst the industrial clamor of a bustling
           port city, a unique and storied football club, FC THG 08, emerged. The
           year was 1893, and the city's docks were alive with the toil of
@@ -14,23 +14,37 @@ const HistoryFounder = () => {
           the game and the palpable sense of unity they observed among the
           dockworkers.
         </p>
-        <p className="history-founder__text paragraph-text mb-sm">
+        <p className="paragraph-text mb-sm">
           As the founding members of FC THG 08, their names reflected the
           mingling of British and German heritage, embodying the cross-cultural
           spirit of the port city.
         </p>
-        <p className="history-founder__text paragraph-text mb-sm">
-          The founders:
-        </p>
+        <p className="history-founder__founders paragraph-text">Founders:</p>
         <ul className="history-founder__list">
-          <li className="history-founder__list--item">William Hartmann</li>
-          <li className="history-founder__list--item">Edward Becker</li>
-          <li className="history-founder__list--item">Tristan Griffiths</li>
-          <li className="history-founder__list--item">Henry Schmidt</li>
-          <li className="history-founder__list--item">Charles Müller</li>
-          <li className="history-founder__list--item">James Klein</li>
-          <li className="history-founder__list--item">Richard Schneider</li>
-          <li className="history-founder__list--item">John Fischer</li>
+          <li className="history-founder__list--item paragraph-text">
+            William Hartmann
+          </li>
+          <li className="history-founder__list--item paragraph-text">
+            Edward Becker
+          </li>
+          <li className="history-founder__list--item paragraph-text">
+            Tristan Griffiths
+          </li>
+          <li className="history-founder__list--item paragraph-text">
+            Henry Schmidt
+          </li>
+          <li className="history-founder__list--item paragraph-text">
+            Charles Müller
+          </li>
+          <li className="history-founder__list--item paragraph-text">
+            James Klein
+          </li>
+          <li className="history-founder__list--item paragraph-text">
+            Richard Schneider
+          </li>
+          <li className="history-founder__list--item paragraph-text">
+            John Fischer
+          </li>
         </ul>
       </div>
     </div>
