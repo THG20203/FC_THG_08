10a9f304e160c3f9881c8c401cbf8b0f08968cf6
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-30 00:02:26 +0100

    more typography file changes

--- frontend/src/components/History/HistoryBadge/HistoryBadge.js
+++ frontend/src/components/History/HistoryBadge/HistoryBadge.js
@@ -6,6 +6,9 @@ const HistoryBadge = () => {
   return (
     <div className="history-badge__container">
       <img src={badge} alt="Badge" />
+      <div className="history-badge__caption">
+        <h2 className="heading-2--light">Caption</h2>
+      </div>
     </div>
   );
 };
