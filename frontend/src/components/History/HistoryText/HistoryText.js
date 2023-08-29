d07e1631e620dc78142e52c7646a26a440c5a241
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-29 14:18:28 +0100

    sorting out title and padding of the history text section

--- frontend/src/components/History/HistoryText/HistoryText.js
+++ frontend/src/components/History/HistoryText/HistoryText.js
@@ -3,7 +3,11 @@ import "./HistoryText.scss";
 const HistoryText = () => {
   return (
     <div className="history-text__container">
-      <div className="">Test</div>
+      <div className="history-text__content">
+        <h1 className="heading-1--light mb-lg">
+          <em>Traditions of FC THG 08:</em> Forging a Legacy on the Pitch
+        </h1>
+      </div>
     </div>
   );
 };
