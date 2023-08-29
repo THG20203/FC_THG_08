774fe1e4f9a6a4111b77226c284b3bfa4d38a190
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-29 23:55:08 +0100

    beginning history badge markup

---
+++ frontend/src/components/History/HistoryBadge/HistoryBadge.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HistoryBadge.scss";
+import "../../../assets/images/identity/football-badge.png";
+
+const HistoryBadge = () => {
+  return (
+    <div className="history-badge__container">
+      <div>Test</div>
+    </div>
+  );
+};
+
+export default HistoryBadge;
