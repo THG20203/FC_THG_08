b517e6691d94257c9c700129b76c170affde5dd2
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-29 13:44:13 +0100

    basic styles of history text added in

--- frontend/src/components/History/LockSlider/LockSlider.js
+++ frontend/src/components/History/LockSlider/LockSlider.js
@@ -4,7 +4,7 @@ import "./LockSlider.scss";
 const LockSlider = ({ handleInput, sliderValue, width }) => {
   let sliderStyle = {
     appearance: "none",
-    width: "30vw",
+    width: "40vw",
     height: "2vh",
     cursor: "pointer",
     borderRadius: "20px",
