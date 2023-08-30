83fee9f553a079cbf9fa4b9856857bcebcbdc57d
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-30 11:54:47 +0100

    finished history section

--- frontend/src/components/History/SerpentHistory/SerpentHistory.js
+++ frontend/src/components/History/SerpentHistory/SerpentHistory.js
@@ -3,7 +3,17 @@ import "./SerpentHistory.scss";
 const SerpentHistory = () => {
   return (
     <div className="sea-serpent__container">
-      <div className="sea-serpent__content">Test</div>
+      <div className="sea-serpent__content">
+        <h1 className="sea-serpent__title heading-1--light mb-sm">Our Logo</h1>
+        <p className="sea-serpent__description paragraph-text">
+          FC THG 08's logo features a sea serpent, a symbol chosen to reflect
+          the deep connection between the football team and its port city's
+          maritime heritage. As a representation of the city's ties to the sea,
+          the sea serpent emblem embodies the club's identity as a unifying
+          force within the community, blending the vigor of the sport with the
+          resilience and mystique of the ocean.
+        </p>
+      </div>
     </div>
   );
 };
