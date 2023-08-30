fa3b0964d891484522e7cfdce52bd1c715cd2619
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-30 15:16:06 +0100

    contuining card rotation work

--- frontend/src/components/Latest/HeadlineCards/Headline.js
+++ frontend/src/components/Latest/HeadlineCards/Headline.js
@@ -7,20 +7,32 @@ const Headline = () => {
       <div className="headline-cards__row">
         <div className="headline-cards__col-1">
           <div className="headline-cards__card">
-            <div className="headline-cards__card-side--front">Card</div>
-            <div className="headline-cards__card-side--back">back</div>
+            <div className="headline-cards__card-side headline-cards__card-side--front">
+              Card
+            </div>
+            <div className="headline-cards__card-side headline-cards__card-side--back">
+              back
+            </div>
           </div>
         </div>
         <div className="headline-cards__col-2">
           <div className="headline-cards__card">
-            <div className="headline-cards__card-side--front">Card</div>
-            <div className="headline-cards__card-side--back">back</div>
+            <div className="headline-cards__card-side headline-cards__card-side--front">
+              Card
+            </div>
+            <div className="headline-cards__card-side headline-cards__card-side--back">
+              back
+            </div>
           </div>
         </div>
         <div className="headline-cards__col-3">
           <div className="headline-cards__card">
-            <div className="headline-cards__card-side--front">Card</div>
-            <div className="headline-cards__card-side--back">back</div>
+            <div className="headline-cards__card-side headline-cards__card-side--front">
+              Card
+            </div>
+            <div className="headline-cards__card-side headline-cards__card-side--back">
+              back
+            </div>
           </div>
         </div>
       </div>
