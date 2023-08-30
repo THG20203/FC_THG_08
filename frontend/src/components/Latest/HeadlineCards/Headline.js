f8b65ed240f60e56539da29f4680df92bbe4c195
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-30 14:33:20 +0100

    cards markup

--- frontend/src/components/Latest/HeadlineCards/Headline.js
+++ frontend/src/components/Latest/HeadlineCards/Headline.js
@@ -3,11 +3,19 @@ import "./Headline.scss";
 
 const Headline = () => {
   return (
-    <div className="headline-cards__row">
-      <div className="headline-cards__col-1">Col 1 of 3</div>
-      <div className="headline-cards__col-2">Col 2 of 3</div>
-      <div className="headline-cards__col-3">Col 3 of 3</div>
-    </div>
+    <section className="headline-cards__section">
+      <div className="headline-cards__row">
+        <div className="headline-cards__col-1">
+          <div className="headline-cards__card">Card</div>
+        </div>
+        <div className="headline-cards__col-2">
+          <div className="headline-cards__card">Card</div>
+        </div>
+        <div className="headline-cards__col-3">
+          <div className="headline-cards__card">Card</div>
+        </div>
+      </div>
+    </section>
   );
 };
 
