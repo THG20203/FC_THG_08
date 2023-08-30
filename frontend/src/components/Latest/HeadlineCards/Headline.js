647de29b099b3c49707c1a0d6d38d4721fbfad3a
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-31 00:38:39 +0100

    some card scss work

--- frontend/src/components/Latest/HeadlineCards/Headline.js
+++ frontend/src/components/Latest/HeadlineCards/Headline.js
@@ -5,7 +5,7 @@ const Headline = () => {
   return (
     <section className="headline-cards__section">
       <div className="headline-cards__row">
-        <div className="headline-cards__col">
+        <div className="headline-cards__col  headline-cards__col--1">
           <div className="headline-cards__card">
             <div className="headline-cards__card-side headline-cards__card-side--front">
               Card
@@ -15,7 +15,7 @@ const Headline = () => {
             </div>
           </div>
         </div>
-        <div className="headline-cards__col">
+        <div className="headline-cards__col headline-cards__col--2">
           <div className="headline-cards__card">
             <div className="headline-cards__card-side headline-cards__card-side--front">
               Card
@@ -25,7 +25,7 @@ const Headline = () => {
             </div>
           </div>
         </div>
-        <div className="headline-cards__col">
+        <div className="headline-cards__col  headline-cards__col--3">
           <div className="headline-cards__card">
             <div className="headline-cards__card-side headline-cards__card-side--front">
               Card
