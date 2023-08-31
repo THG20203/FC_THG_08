1454b15043be7f8eb478e1ac97318e05d4f359b6
THG20203 <tristanhgriffiths8@yahoo.com>
2023-08-31 22:24:45 +0100

    useRef and theory for video

--- frontend/src/components/Home/Video/Video.js
+++ frontend/src/components/Home/Video/Video.js
@@ -1,12 +1,37 @@
+import React, { useState, useRef } from "react";
 import "./Video.scss";
 import trainingVideo from "../../../assets/videos/training-video.mp4";
 
 const Video = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  /* useRef allows me to create a mutable reference to a DOM element or any value 
+  the persists across renders without causing re-renders when the reference changes. */
+  /* useRef() -> access / interact with DOM elements holds values I want to persist with between 
+  renders without causing re render */
+  const videoRef = useRef(null);
+
+  const togglePlay = () => {
+    const video = videoRef.current;
+    if (video) {
+      if (isPlaying) {
+        video.pause();
+      } else {
+        video.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
   return (
     <div className="video__container">
-      <video muted loop autoplay>
-        <source src={trainingVideo} type="video/mp4"></source>
-      </video>
+      <div className="video__content">
+        <video ref={videoRef} className="video__player" onClick={togglePlay}>
+          <source src={trainingVideo} type="video/mp4"></source>
+        </video>
+        <button className="play-button" onClick={togglePlay}>
+          {/* if isPlaying, Pause, if isn't -> play */}
+          {isPlaying ? "Pause" : "Play"}
+        </button>
+      </div>
     </div>
   );
 };
