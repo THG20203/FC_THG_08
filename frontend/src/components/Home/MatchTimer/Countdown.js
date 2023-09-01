55c7068c81502855d68f377fbcd04b8a912158b4
THG20203 <tristanhgriffiths8@yahoo.com>
2023-09-01 16:10:43 +0100

    some comments written

--- frontend/src/components/Home/MatchTimer/Countdown.js
+++ frontend/src/components/Home/MatchTimer/Countdown.js
@@ -1,23 +1,38 @@
 import React, { useState, useEffect } from "react";
 
 const Countdown = () => {
+  /* useState hook to create a state variable timeLeft and a function setTimeLeft to 
+  update its value. The initial value of timeLeft is set to the result of the 
+  getTimeUntilNextSaturday3PM function, which calculates the time remaining until 
+  the next Saturday at 3 PM. */
   const [timeLeft, setTimeLeft] = useState(getTimeUntilNextSaturday3PM());
 
-  // Function to update the timer
+  /* Function to update the timer. updateTimer is defined as a function 
+  that sets the timeLeft state variable by calling the getTimeUntilNextSaturday3PM
+  function. This function will be used to update the timer periodically. */
   const updateTimer = () => {
     setTimeLeft(getTimeUntilNextSaturday3PM());
   };
 
-  // Use useEffect to start the timer and clear it when timeLeft reaches 0
+  /* This block of code uses the useEffect hook to manage the timer. It starts by 
+  setting up a timer using setInterval that calls the updateTimer function every 
+  1000 milliseconds (1 second). */
   useEffect(() => {
     const timer = setInterval(updateTimer, 1000);
 
-    // Clear the timer when timeLeft is 0
+    /* The useEffect hook also returns a cleanup function, which is used to clear
+    the timer when the component unmounts or when the dependencies change. In this case,
+    there are no dependencies specified (empty dependency array []), so the cleanup 
+    function will be called when the component unmounts. */
     return () => clearInterval(timer);
   }, []);
 
   // Calculate the time remaining until the next Saturday at 3 PM
   function getTimeUntilNextSaturday3PM() {
+    /* This is a function getTimeUntilNextSaturday3PM that calculates the time remaining until the next Saturday at 3 PM.
+    It uses the Date object to get the current date and time. It calculates the number of days, hours, 
+    minutes, and seconds remaining until the next Saturday at 3 PM by subtracting the current values
+    from the target values (6 for Saturday, 15 for 3 PM, 0 for minutes, and 0 for seconds). */
     const now = new Date();
     const daysUntilSaturday = 6 - now.getDay(); // 6 is Saturday
     const hoursUntil3PM = 15 - now.getHours(); // 15 is 3 PM
@@ -25,6 +40,7 @@ const Countdown = () => {
     const secondsUntil3PM = 0 - now.getSeconds(); // 0 is the second value for 3 PM
 
     return (
+      /* 24 hours in a day, 60 minutes in an hour, 60 seconds in a minute */
       daysUntilSaturday * 24 * 60 * 60 +
       hoursUntil3PM * 60 * 60 +
       minutesUntil3PM * 60 +
@@ -32,11 +48,15 @@ const Countdown = () => {
     );
   }
 
-  // Format the time as days, hours, minutes, and seconds
+  /* Format the time as days, hours, minutes, and seconds */
   const days = Math.floor(timeLeft / (24 * 60 * 60));
   const hours = Math.floor((timeLeft % (24 * 60 * 60)) / (60 * 60));
   const minutes = Math.floor((timeLeft % (60 * 60)) / 60);
   const seconds = timeLeft % 60;
+
+  /* In summary, this code sets up a timer that updates the
+  timeLeft state variable every second, counting down to the next Saturday
+  at 3 PM. When the component unmounts, it clears the timer to prevent memory leaks. */
 
   return (
     <div>
