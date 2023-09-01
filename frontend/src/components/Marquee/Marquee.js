1eb7b81dea3b4da9bcf7af339eee0e9fb3a8395f
THG20203 <tristanhgriffiths8@yahoo.com>
2023-09-01 02:08:38 +0100

    images added to marquee element

--- frontend/src/components/Marquee/Marquee.js
+++ frontend/src/components/Marquee/Marquee.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./Marquee.scss";
+import ePayel from "../../assets/images/logos/e-payel.png";
+import foliole from "../../assets/images/logos/foliole.png";
+import shalbourne from "../../assets/images/logos/shalbourne.png";
+import subiro from "../../assets/images/logos/subiro.png";
+import sword from "../../assets/images/logos/sword.png";
+import ultramarine from "../../assets/images/logos/ultramarine.png";
 
 const Marquee = () => {
   return (
     <div className="marquee__container">
-      <div className="marquee__content">test</div>
+      <div className="marquee__content">
+        <img src={ePayel} alt="E payel logo" />
+        <img src={foliole} alt="Foliole logo" />
+        <img src={shalbourne} alt="Shalbourne logo" />
+        <img src={subiro} alt="Subiro logo" />
+        <img src={sword} alt="Sword logo" />
+        <img src={ultramarine} alt="Ultramarine logo" />
+      </div>
     </div>
   );
 };
