42710ff1f18385de99f4bc7d795cb948c9beb320
THG20203 <tristanhgriffiths8@yahoo.com>
2023-09-03 16:58:15 +0100

    markup correction

--- frontend/src/components/Home/MatchInformation/NextMatch/NextMatch.js
+++ frontend/src/components/Home/MatchInformation/NextMatch/NextMatch.js
@@ -5,7 +5,7 @@ import eagleBadge from "../../../../assets/images/matchBadges/eagle-badge.png";
 const NextMatch = () => {
   return (
     <div className="next-match__container">
-      <p>Last Match</p>
+      <p>Next Match</p>
       <img src={blueBadge} alt="FC THG 08 Badge" />
       <img src={eagleBadge} alt="ECFC Badge" />
     </div>
