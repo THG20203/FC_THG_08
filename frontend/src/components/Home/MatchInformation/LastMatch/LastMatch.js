1bf5a5b7d6f4368109b724b13f2ea21574eb1b2e
THG20203 <tristanhgriffiths8@yahoo.com>
2023-09-03 16:57:46 +0100

    sorting some markup for last match and next

--- frontend/src/components/Home/MatchInformation/LastMatch/LastMatch.js
+++ frontend/src/components/Home/MatchInformation/LastMatch/LastMatch.js
@@ -1,7 +1,15 @@
 import "./LastMatch.scss";
+import blueBadge from "../../../../assets/images/identity/football-badge.png";
+import redBadge from "../../../../assets/images/matchBadges/football-badge-red.png";
 
 const LastMatch = () => {
-  return <div className="last-match__container">test</div>;
+  return (
+    <div className="last-match__container">
+      <p>Last Match</p>
+      <img src={blueBadge} alt="FC THG 08 Badge" />
+      <img src={redBadge} alt="Crimson Pangmere Badge" />
+    </div>
+  );
 };
 
 export default LastMatch;
--- frontend/src/components/Home/MatchInformation/NextMatch/NextMatch.js
+++ frontend/src/components/Home/MatchInformation/NextMatch/NextMatch.js
@@ -1,7 +1,15 @@
 import "./NextMatch.scss";
+import blueBadge from "../../../../assets/images/identity/football-badge.png";
+import eagleBadge from "../../../../assets/images/matchBadges/eagle-badge.png";
 
 const NextMatch = () => {
-  return <div className="next-match__container">test</div>;
+  return (
+    <div className="next-match__container">
+      <p>Last Match</p>
+      <img src={blueBadge} alt="FC THG 08 Badge" />
+      <img src={eagleBadge} alt="ECFC Badge" />
+    </div>
+  );
 };
 
 export default NextMatch;
