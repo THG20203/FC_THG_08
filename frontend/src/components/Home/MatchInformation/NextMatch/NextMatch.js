170c0c85749e195b860b36158d913299ec28261c
THG20203 <tristanhgriffiths8@yahoo.com>
2023-09-03 15:37:54 +0100

    deciding on more color schemes

---
+++ frontend/src/components/Home/MatchInformation/NextMatch/NextMatch.js
@@ -0,0 +1,7 @@
+import "./NextMatch.scss";
+
+const NextMatch = () => {
+  return <div className="next-match__container">test</div>;
+};
+
+export default NextMatch;
--- frontend/src/pages/whiteNavPages/Home.js
+++ frontend/src/pages/whiteNavPages/Home.js
@@ -4,6 +4,7 @@ import Carousel from "../../components/Home/Carousel/Carousel";
 import Video from "../../components/Home/Video/Video";
 import LastMatch from "../../components/Home/MatchInformation/LastMatch/LastMatch";
 import MatchTimer from "../../components/Home/MatchInformation/MatchTimer/MatchTimer";
+import NextMatch from "../../components/Home/MatchInformation/NextMatch/NextMatch";
 import Marquee from "../../components/Marquee/Marquee";
 import Features from "../../components/Home/Features/Features";
 
@@ -14,6 +15,7 @@ const HomePage = () => {
       <Video />
       <LastMatch />
       <MatchTimer />
+      <NextMatch />
       <Marquee />
       <Features />
     </div>
