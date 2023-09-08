09b802b36109bca1180b28a0f53028f3edc8e6c0
THG20203 <tristanhgriffiths8@yahoo.com>
2023-09-08 14:55:04 +0100

    adding more markup and selectors in css and blank space for image eleement

--- frontend/src/components/Latest/HeadlineCards/Headline.js
+++ frontend/src/components/Latest/HeadlineCards/Headline.js
@@ -9,7 +9,11 @@ const Headline = () => {
           <div className="headline-cards__col  headline-cards__col--1">
             <div className="headline-cards__card">
               <div className="headline-cards__card-side headline-cards__card-side--front">
+                <div className="headline-cards__card--picture headline-cards__card--picture--1">
+                  &nbsp;
+                </div>
                 <div className="headline-cards__card--heading">Heading</div>
+                <div className="headline-cards__card--details">Details</div>
               </div>
               <div className="headline-cards__card-side headline-cards__card-side--back">
                 back
@@ -19,7 +23,11 @@ const Headline = () => {
           <div className="headline-cards__col headline-cards__col--2">
             <div className="headline-cards__card">
               <div className="headline-cards__card-side headline-cards__card-side--front">
+                <div className="headline-cards__card--picture headline-cards__card--picture--2">
+                  &nbsp;
+                </div>
                 <div className="headline-cards__card--heading">Heading</div>
+                <div className="headline-cards__card--details">Details</div>
               </div>
               <div className="headline-cards__card-side headline-cards__card-side--back">
                 back
@@ -29,7 +37,11 @@ const Headline = () => {
           <div className="headline-cards__col  headline-cards__col--3">
             <div className="headline-cards__card">
               <div className="headline-cards__card-side headline-cards__card-side--front">
+                <div className="headline-cards__card--picture headline-cards__card--picture--3">
+                  &nbsp;
+                </div>
                 <div className="headline-cards__card--heading">Heading</div>
+                <div className="headline-cards__card--details">Details</div>
               </div>
               <div className="headline-cards__card-side headline-cards__card-side--back">
                 back
