eb6ec120e25749b1a7e39baea60bff5a7c02c2f2
THG20203 <tristanhgriffiths8@yahoo.com>
2023-09-13 15:28:46 +0100

    more players grid styles

--- frontend/src/components/Home/Players/Players.js
+++ frontend/src/components/Home/Players/Players.js
@@ -11,17 +11,17 @@ const Players = () => {
         <img className="players__img" src={Player1} alt="Ishmael Baptiste" />
         <div className="players__details">
           <h4 className="heading-4--light">Ishmael Baptiste</h4>
-          <p className="players__stats paragraph-text">Example Text</p>
+          <p className="players__stats paragraph-text">10 apps, 6 goals</p>
         </div>
         <img className="players__img" src={Player2} alt="Frank Carter" />
         <div className="players__details">
           <h4 className="heading-4--light">Frank Carter</h4>
-          <p className="players__stats paragraph-text">Example Text</p>
+          <p className="players__stats paragraph-text">9 apps, 5 goals</p>
         </div>
         <img className="players__img" src={Player3} alt="Thomas Mahrez" />
         <div className="players__details">
           <h4 className="heading-4--light">Thomas Mahrez</h4>
-          <p className="players__stats paragraph-text">Example Text</p>
+          <p className="players__stats paragraph-text">10 apps, 4 goals</p>
         </div>
       </div>
     </div>
