7c6fe584ce17aa35d290e0d0ba3ee7a6aed8e65d
THG20203 <tristanhgriffiths8@yahoo.com>
2023-09-13 15:26:08 +0100

    creating list grid for the players

--- frontend/src/components/Home/Players/Players.js
+++ frontend/src/components/Home/Players/Players.js
@@ -11,17 +11,17 @@ const Players = () => {
         <img className="players__img" src={Player1} alt="Ishmael Baptiste" />
         <div className="players__details">
           <h4 className="heading-4--light">Ishmael Baptiste</h4>
-          <p className="players__stats">Example Text</p>
+          <p className="players__stats paragraph-text">Example Text</p>
         </div>
         <img className="players__img" src={Player2} alt="Frank Carter" />
         <div className="players__details">
           <h4 className="heading-4--light">Frank Carter</h4>
-          <p className="players__stats">Example Text</p>
+          <p className="players__stats paragraph-text">Example Text</p>
         </div>
         <img className="players__img" src={Player3} alt="Thomas Mahrez" />
         <div className="players__details">
           <h4 className="heading-4--light">Thomas Mahrez</h4>
-          <p className="players__stats">Example Text</p>
+          <p className="players__stats paragraph-text">Example Text</p>
         </div>
       </div>
     </div>
