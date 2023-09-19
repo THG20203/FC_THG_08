877c9eb5c2201ce85efc7932c094151ddf4f4320
THG20203 <tristanhgriffiths8@yahoo.com>
2023-09-19 13:00:37 +0100

    further alterations to the history page

--- frontend/src/components/History/HistoryText/HistoryText.js
+++ frontend/src/components/History/HistoryText/HistoryText.js
@@ -9,7 +9,7 @@ const HistoryText = () => {
         </h1>
         <div className="history-text__two-column">
           <h4 className="heading-4--light history-text__description history-text__dates">
-            <span>1893 - 1920</span>
+            <span>1893 - 1919</span>
           </h4>
           <p className="paragraph-text history-text__description">
             The club's early years were marked by gritty determination. From
@@ -19,11 +19,21 @@ const HistoryText = () => {
         </div>
         <div className="history-text__two-column">
           <h4 className="heading-4--light history-text__description history-text__dates">
-            <span>1920 - 1977</span>
+            <span>1920 - 1957</span>
           </h4>
           <p className="paragraph-text history-text__description">
-            FC THG 08 secured 10 league titles from 1920 to 1977. Their
-            consistency over nearly six decades gained national attention.
+            FC THG 08 secured 10 league titles from 1920 to 1957. Their
+            consistency over nearly four decades gained national attention.
+          </p>
+        </div>
+        <div className="history-text__two-column">
+          <h4 className="heading-4--light history-text__description history-text__dates">
+            <span>1958 - 1976</span>
+          </h4>
+          <p className="paragraph-text history-text__description">
+            The club played in Europe for the first time in the late 50's and
+            early 60's, challenging the continents best for international
+            honours.
           </p>
         </div>
         <div className="history-text__two-column">
