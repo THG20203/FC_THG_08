184bfecc00fb01b19fa444ba7feadda4eea99b2b
THG20203 <tristanhgriffiths8@yahoo.com>
2023-09-19 16:17:10 +0100

    latest feature import sorted

---
+++ frontend/src/components/Latest/LatestFeature/LatestFeature.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LatestFeature.scss";
+
+const LatestFeature = () => {
+  return <div className="latest-feature__container">Test</div>;
+};
+
+export default LatestFeature;
--- frontend/src/pages/whiteNavPages/Latest.js
+++ frontend/src/pages/whiteNavPages/Latest.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Latest.scss";
 import NotableStories from "../../components/Latest/NotableStories/NotableStories";
 import Headline from "../../components/Latest/HeadlineCards/Headline";
+import LatestFeature from "../../components/Latest/LatestFeature/LatestFeature";
 import News from "../../components/Latest/News/News";
 
 const LatestPage = () => {
@@ -9,6 +10,7 @@ const LatestPage = () => {
     <div className="latest-container">
       <NotableStories />
       <Headline />
+      <LatestFeature />
       <News />
     </div>
   );
