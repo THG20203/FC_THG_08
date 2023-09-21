013709395dc671896d707171d636bb0b0e3de066
THG20203 <tristanhgriffiths8@yahoo.com>
2023-09-21 12:57:54 +0100

    going through the fixture list

--- frontend/src/components/Home/MatchInformation/LastMatch/LastMatch.js
+++ frontend/src/components/Home/MatchInformation/LastMatch/LastMatch.js
@@ -1,15 +1,17 @@
 import "./LastMatch.scss";
-import blueBadge from "../../../../assets/images/identity/football-badge.png";
+import whiteBadge from "../../../../assets/images/identity/football-badge-white.png";
 import redBadge from "../../../../assets/images/matchBadges/football-badge-red.png";
 
 const LastMatch = () => {
   return (
     <div className="last-match__container">
-      <h4 className="heading-4 fixture__title">Last Match: Wednesday 8pm</h4>
+      <h4 className="heading-4--light fixture__title">
+        Last Match: Wednesday 8pm
+      </h4>
       <div className="fixture__content">
         <img
           className="last-match__badge--blue"
-          src={blueBadge}
+          src={whiteBadge}
           alt="FC THG 08 Badge"
         />
         <h1 className="heading-1 fixture__score">3 : 0</h1>
--- frontend/src/components/Home/MatchInformation/NextMatch/NextMatch.js
+++ frontend/src/components/Home/MatchInformation/NextMatch/NextMatch.js
@@ -1,18 +1,23 @@
 import "./NextMatch.scss";
-import blueBadge from "../../../../assets/images/identity/football-badge.png";
+import whiteBadge from "../../../../assets/images/identity/football-badge-white.png";
 import eagleBadge from "../../../../assets/images/matchBadges/eagle-badge.png";
 
 const NextMatch = () => {
   return (
     <div className="next-match__container">
-      <h4 className="heading-4 fixture__title">Next Match: Saturday 3pm</h4>
+      <h4 className="heading-4--light fixture__title">
+        Next Match: Saturday 3pm
+      </h4>
       <div className="fixture__content">
-        <img
-          className="next-match__badge--blue"
-          src={blueBadge}
-          alt="FC THG 08 Badge"
-        />
-        <h1 className="heading-1 fixture__score">- : -</h1>
+        <div className="fixture__club">
+          <img
+            className="next-match__badge--blue"
+            src={whiteBadge}
+            alt="FC THG 08 Badge"
+          />
+          <p className="fixture__club--name paragraph-text">FC THG 08</p>
+        </div>
+        <h1 className="heading-1 fixture__score next-match__score">- : -</h1>
         <img
           className="next-match__badge--eagle"
           src={eagleBadge}
