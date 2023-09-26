b087fd8eb9430e0f63c10994e21749b70e2d4008
THG20203 <tristanhgriffiths8@yahoo.com>
2023-09-26 23:57:28 +0100

    football silhouette added

--- frontend/src/components/Home/MatchInformation/MatchTimer/MatchTimer.js
+++ frontend/src/components/Home/MatchInformation/MatchTimer/MatchTimer.js
@@ -2,12 +2,20 @@ import React from "react";
 import Countdown from "./Countdown";
 import "./MatchTimer.scss";
 import footballPitch from "../../../../assets/images/footballPitch/football-pitch.png";
+import footballerSilhouette from "../../../../assets/images/footballPitch/footballer-silhouette.png";
 
 const MatchTimer = () => {
   return (
     <div className="match-timer__container">
       <div className="match-timer__image-div">
         <img className="match-timer__img" src={footballPitch} alt="Pitch" />
+      </div>
+      <div className="match-timer__image-div">
+        <img
+          className="match-timer__img--2"
+          src={footballerSilhouette}
+          alt="Pitch"
+        />
       </div>
       <Countdown />
     </div>
