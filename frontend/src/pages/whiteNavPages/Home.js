10a6cc53c188be62288d68f744bc01634a62f4e7
THG20203 <tristanhgriffiths8@yahoo.com>
2023-11-11 15:36:02 +0000

    import test

--- frontend/src/pages/whiteNavPages/Home.js
+++ frontend/src/pages/whiteNavPages/Home.js
@@ -5,7 +5,8 @@ import Video from "../../components/Home/Video/Video";
 import LastMatch from "../../components/Home/MatchInformation/LastMatch/LastMatch";
 import MatchTimer from "../../components/Home/MatchInformation/MatchTimer/MatchTimer";
 import NextMatch from "../../components/Home/MatchInformation/NextMatch/NextMatch";
-import Marquee from "../../components/Marquee/Marquee";
+import Marquee from "../../components/Home/Marquee/Marquee";
+import Stadium from "../../components/Home/Stadium/Stadium";
 import Players from "../../components/Home/Players/Players";
 import Headline from "../../components/Latest/HeadlineCards/Headline";
 import Features from "../../components/Home/Features/Features";
@@ -19,6 +20,7 @@ const HomePage = () => {
       <MatchTimer />
       <NextMatch />
       <Marquee />
+      <Stadium />
       <Players />
       <Headline />
       <Features />
