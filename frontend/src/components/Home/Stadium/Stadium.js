50c9e0a04795e4c86999482cc01622cc08ef066f
THG20203 <tristanhgriffiths8@yahoo.com>
2023-11-11 16:01:42 +0000

    sorting image imports

--- frontend/src/components/Home/Stadium/Stadium.js
+++ frontend/src/components/Home/Stadium/Stadium.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Stadium.scss";
+import mural from "../../../assets/images/stadium/stadium.png";
+/* logo imports */
+import ePayel from "../../../assets/images/logos/e-payel.png";
+import foliole from "../../../assets/images/logos/foliole.png";
+import shalbourne from "../../../assets/images/logos/shalbourne.png";
+import subiro from "../../../assets/images/logos/subiro.png";
+import sword from "../../../assets/images/logos/sword.png";
+import ultramarine from "../../../assets/images/logos/ultramarine.png";
 
 const Stadium = () => {
-  return <div>Stadium test</div>;
+  return (
+    <div className="stadium__container">
+      <div className="stadium__sponsors"></div>
+    </div>
+  );
 };
 
 export default Stadium;
